03dc73f6182ba684fd16fedcf13ec2e92501fe23
matheusreis777 <matheuscronapp@gmail.com>
2022-07-13 11:02:09 -0400

    modified:   package-lock.json 	modified:   package.json 	modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,47 @@
-function Home() {
-    return <div>
-        <h3>Home</h3>
-    </div>
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText
+} from 'reactstrap';
 
+const Headers = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
 
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar  style={{ backgroundColor: "#20283b" }} dark expand="md">
+                <NavbarBrand href="/">Logo</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/">Quem Somos</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/">Serviços</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/">Contato</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    );
 }
 
-export default Home
+
+export default Headers;
